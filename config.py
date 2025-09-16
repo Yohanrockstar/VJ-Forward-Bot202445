34c1460cce0041c45a983d33b6c0937c88b57aab
ʟᴏɴᴇʟʏ ʙᴏʏ <117445045+Yohanrockstar@users.noreply.github.com>
2025-09-16 06:50:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ from os import environ
 class Config:
     API_ID = int(environ.get("API_ID", "1220805"))
     API_HASH = environ.get("API_HASH", "b01d18cba6e1b7f206cc2d3d415ceed1")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "5580538946:AAFKWNQfyL--YKq7utxi8ea1qNQgwXMke_s") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "leech4Robot") 
     DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://Yohanrock:yohanrockstar@cluster0.v8tewcp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "cluster0")
@@ -27,4 +27,3 @@ class temp(object):
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
-
